2c67118a262c753446bab4e068f3f324c42a40c0
kimtaeo <kimtae5@naver.com>
2022-06-27 15:50:30 +0900

    20220627-1

---
+++ app/src/main/java/com/kt5/mobilefront1/StartActivity.java
@@ -0,0 +1,32 @@
+package com.kt5.mobilefront1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+       super.onCreate(savedInstanceState);
+       Intent intent = new Intent(
+                StartActivity.this, MainActivity.class);
+       startActivity(intent);
+       finish();
+
+       /*
+       setContentView(R.layout.activity_start);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(
+                        StartActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },1000*3);
+        */
+    }
+}
